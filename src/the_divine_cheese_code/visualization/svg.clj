b95d1cbea24867f4b74f6bce28b749f1f03dfc3f
Rusty Bentley <mycoding.alert@gmail.com>
2015-12-07 18:36:26 -0600

    refactored to return lat,lng and added main implementation

--- src/the_divine_cheese_code/visualization/svg.clj
+++ src/the_divine_cheese_code/visualization/svg.clj
@@ -3,9 +3,9 @@
 (defn latlng->point
   "Given a map containing lat and lng, returns those elements as a comma-delimited string of lng,lat."
   [{:keys [lat lng]}]
-  (str lng "," lat))
+  (str lat "," lng))
 
 (defn points
-  "Given a sequence of locaiton maps, returns the latlng points as a space-delimited string"
+  "Given a sequence of location maps, returns the latlng points as a space-delimited string"
   [locations]
   (clojure.string/join " " (map latlng->point locations)))
--- test/the_divine_cheese_code/visualization/svg_test.clj
+++ test/the_divine_cheese_code/visualization/svg_test.clj
@@ -5,10 +5,10 @@
 
 (deftest latlng->point-test
   (testing "Given a map containing lat and lng, returns those elements as a comma-delimited string"
-    (is (= "6.97,50.95" (latlng->point {:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97})))))
+    (is (= "50.95,6.97" (latlng->point {:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97})))))
 
 (deftest points-test
   (testing "Given a sequence of location maps, returns the latlng points as a space-delimited string"
-    (is (= "6.97,50.95" (points [{:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97}])))
-    (is (= "6.97,50.95 3.14,15.97" (points [{:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97}
+    (is (= "50.95,6.97" (points [{:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97}])))
+    (is (= "50.95,6.97 15.97,3.14" (points [{:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97}
                                             {:location "place2" :cheese-name "cheese2" :lat 15.97 :lng 3.14}])))))
