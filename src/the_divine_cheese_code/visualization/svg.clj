722d39fa48476e6d5ef4ab94e0feea716f1d140a
Rusty Bentley <mycoding.alert@gmail.com>
2015-12-07 19:41:48 -0600

    implementing min and max for points collections

--- src/the_divine_cheese_code/visualization/svg.clj
+++ src/the_divine_cheese_code/visualization/svg.clj
@@ -1,4 +1,15 @@
-(ns the-divine-cheese-code.visualization.svg)
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as str])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks (map (fn [k] (apply comparison-fn (map k maps))) ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
 
 (defn latlng->point
   "Given a map containing lat and lng, returns those elements as a comma-delimited string of lng,lat."
@@ -8,4 +19,4 @@
 (defn points
   "Given a sequence of location maps, returns the latlng points as a space-delimited string"
   [locations]
-  (clojure.string/join " " (map latlng->point locations)))
+  (str/join " " (map latlng->point locations)))
--- test/the_divine_cheese_code/visualization/svg_test.clj
+++ test/the_divine_cheese_code/visualization/svg_test.clj
@@ -1,6 +1,7 @@
 (ns the-divine-cheese-code.visualization.svg-test
-  (require [clojure.test :refer :all]
-           [the-divine-cheese-code.visualization.svg :refer :all]))
+  (:require [clojure.test :refer :all]
+            [the-divine-cheese-code.visualization.svg :refer :all])
+  (:refer-clojure :exclude [min max]))
 
 
 (deftest latlng->point-test
@@ -12,3 +13,9 @@
     (is (= "50.95,6.97" (points [{:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97}])))
     (is (= "50.95,6.97 15.97,3.14" (points [{:location "place1" :cheese-name "cheese1" :lat 50.95 :lng 6.97}
                                             {:location "place2" :cheese-name "cheese2" :lat 15.97 :lng 3.14}])))))
+
+(deftest min-test
+  (is (= {:lat 1 :lng 0} (min [{:lat 1 :lng 3} {:lat 5 :lng 0}]))))
+
+(deftest max-test
+  (is (= {:lat 5 :lng 3} (max [{:lat 1 :lng 3} {:lat 5 :lng 0}]))))
